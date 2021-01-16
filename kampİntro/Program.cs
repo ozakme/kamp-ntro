335adaa9d284d73d3ba3211e2000f159055864f8
mehmet akif özbek <ozbekmehmetakif1@outlook.com.tr>
2021-01-16 23:47:04 +0300

    16 ocak ders sonu

---
+++ kampİntro/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace kampİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            // Do not repeat yourself- Kendini tekrarlama
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisyapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu ");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu ");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu ");
+            }
+
+            if (sistemeGirisyapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarlar butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+            
+         
+        }
+    }
+}
---
+++ ternanyOperratoru/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ternanyOperratoru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ternary Operatörü
+            Console.Write("[1-10] arasında sayı giriniz :");
+            int sayi = int.Parse(Console.ReadLine());
+            string cevap = "";
+            cevap += sayi == 7 ? "Tebrikler doğru bildiniz" : "Yanlış cevap";
+            Console.WriteLine(cevap);
+
+            // Coalescing Kullanımı
+            Console.Write("Lütfen yaşınızı giriniz :");
+            string yasiniz = Console.ReadLine();
+            string sonuc = "";
+            //Burada iki operatörü birarada kullanmış olduk 
+            //Ternary kullanma amaçımız kullanıcı direkt boş geçerse stringlerde boş değer bir karakter olduğudan
+            //Coalescing de bildiğiniz gibi null değer gelirse varsayılan değeri yazdırır.
+            sonuc = (yasiniz == "" ? null : yasiniz) ?? "Boş geçildi.";
+            Console.WriteLine("Yaşınız :" + sonuc);
+        }
+    }
+}
