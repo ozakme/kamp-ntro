f92aa6f9ebed1486e24f30d75679a3cc53294e53
mehmet akif özbek <ozbekmehmetakif1@outlook.com.tr>
2021-01-20 23:50:25 +0300

    20 ocak ders sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //snippet = hazır kodlar 
+    class Product 
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnıtPrice { get; set; }
+        //stok adedi ||
+        public int UnitsInStock { get; set; }
+        //CRUD ekleme silme işlemlerinin genel adı 
+
+
+
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+           
+        }
+
+        public void Uptade(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+       
+        
+        
+        /* public int Topla(int sayi1, int sayi2)
+        {//sistemde tutulur daha sonra çarp bölme vs.. kullanılabilir
+            return sayi1 + sayi2;
+        }
+        public void Topla2(int sayi1, int sayi2)
+        {//sistemde tutulmaz sadece yazılır bırakılır
+            Console.WriteLine(sayi1 + sayi2); 
+        }*/
+
+    }
+}
